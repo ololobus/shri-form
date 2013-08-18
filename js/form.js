ecdca91cccb37f8c1cf21dbd212705b1107373ef
Alex K <alex.ololo@yandex.ru>
2013-08-18 21:55:30 +0400

    Предварительная верстка

--- js/form.js
+++ js/form.js
@@ -7,20 +7,23 @@ window.form = {
     this.questions_container = $('#questions-container');
     this.question_template = Handlebars.compile($("#question-template").html());
     
-    $('.form-group-title').on('click', function() {
-      $(this).siblings('.form-group-content').slideToggle(200);
+    $('.form-group-title h3').on('click', function() {
+      var self = this;
+      $(this).closest('.form-group-title').siblings('.form-group-content').slideToggle(200, function() {
+        $(self).children('.chevron-icon').toggleClass('down');
+      });
     });
   },
   
   render: function() {
-    self = this;
+    var self = this;
     $.each(this.questions_proto, function(i, q) {
       self.questions_container.append(self.question_template(q));
     });
   },
   
   check: function() {
-    
+    return false;
   }
   
 };
